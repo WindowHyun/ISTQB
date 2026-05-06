8d9d9ba63a7aa559eabace04d32a7ec44c99f997
YyyyyyCH <dusckd4948@naver.com>
2026-05-06 16:43:20 +0900

    Fix Android system bar overlap

--- android/app/src/main/java/com/local/istqbfl/MainActivity.java
+++ android/app/src/main/java/com/local/istqbfl/MainActivity.java
@@ -1,5 +1,36 @@
 package com.local.istqbfl;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    configureSystemBars();
+    super.onCreate(savedInstanceState);
+  }
+
+  private void configureSystemBars() {
+    Window window = getWindow();
+    window.setStatusBarColor(Color.rgb(245, 247, 242));
+    window.setNavigationBarColor(Color.rgb(245, 247, 242));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      window.setDecorFitsSystemWindows(true);
+    }
+
+    int flags = window.getDecorView().getSystemUiVisibility();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+    }
+    window.getDecorView().setSystemUiVisibility(flags);
+  }
+}
